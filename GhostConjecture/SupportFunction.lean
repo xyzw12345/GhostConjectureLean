1ce39daddf2b4819f9a1afd6130f6e0e6847b834
xyzw12345 <wangjt2020@163.com>
2026-02-04 11:36:03 +0800

    revert changes

--- GhostConjecture/SupportFunction.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib/Data/EReal/Basic
-
-namespace GhostConjecture
-
-/-- Support functional for lower convex sets. -/
-noncomputable def supportFunctionLower (C : Set (ℝ × ℝ)) (λ : ℝ) : EReal :=
-  sInf (Real.toEReal '' ((fun p : ℝ × ℝ => p.2 + λ * p.1) '' C))
-
-end GhostConjecture
